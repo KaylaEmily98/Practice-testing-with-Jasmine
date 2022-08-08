9b9097b25686153dab75b175700f778295ac8035
KaylaEmily98 <110258151+KaylaEmily98@users.noreply.github.com>
2022-08-08 11:43:29 -0400

    Add files via upload

---
+++ reduce.js
@@ -0,0 +1,13 @@
+const arr =[{name:"Elie"},{name:'Tim'},{name:'Matt'},{name:'Colt'}]
+function extractValue(arr, key){
+acc.push(next[key]);
+return acc;
+}
+
+function vowelCount(str){
+    const vowels ="aeiou";
+    return str. split('') .reduce (function9acc,next){
+        let lowerCased = next.toLowerCase()
+        if (vowels.indexOf(lower ))
+    }
+}
